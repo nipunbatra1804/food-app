d5dea5bf29b43466ff9fede99d42bcd7079814cd
nipunbatra1994@gmail.com <nipunbatra1994@gmail.com>
2019-02-26 17:07:55 +0800

    add tests for filterbar in HomePage

--- src/components/FilterBar/FilterBar.test.js
+++ src/components/FilterBar/FilterBar.test.js
@@ -1,7 +1,8 @@
 import "jest-dom/extend-expect";
 import "react-testing-library/cleanup-after-each";
+
 import React from "react";
-import { render } from "react-testing-library";
+import { cleanup, render } from "react-testing-library";
 import FilterBar from "./FilterBar";
 
 const data = [
@@ -37,4 +38,34 @@ test("FilterBars renders selected cuisine ID ", () => {
 
   //debug();
   expect(getByText(/japanese/i)).toHaveClass("active");
+});
+
+test("FilterBars renders selectedCuisineID change/mod ", () => {
+  const selectedCuisine1 = {
+    _id: "0010",
+    name: "Japanese"
+  };
+  const firstRender = render(
+    <FilterBar cuisinesList={data} selectedCuisineId={selectedCuisine1._id}>
+      {" "}
+    </FilterBar>
+  );
+
+  //debug();
+
+  expect(firstRender.getByText(/japanese/i)).toHaveClass("active");
+  cleanup();
+  const selectedCuisine2 = {
+    _id: "0011",
+    name: "Western"
+  };
+  const secondRender = render(
+    <FilterBar cuisinesList={data} selectedCuisineId={selectedCuisine2._id}>
+      {" "}
+    </FilterBar>
+  );
+
+  //debug();
+  expect(secondRender.getByText(/Western/i)).toHaveClass("active");
+  //expect(secondRender.getByText(/japanese/i)).toHaveClass("active");
 });
--- src/components/HomePage/HomePage.test.js
+++ src/components/HomePage/HomePage.test.js
@@ -1,7 +1,7 @@
 import "jest-dom/extend-expect";
 import "react-testing-library/cleanup-after-each";
 import React from "react";
-import { render } from "react-testing-library";
+import { render, fireEvent } from "react-testing-library";
 import HomePage from "./HomePage";
 import * as RestaurantService from "../../services/restaurantService";
 
@@ -23,6 +23,28 @@ const sampleData = [
     closingTime: "9:30 PM",
     cuisine: { _id: "5c3430ecfc13ae122d000000", name: "Western" },
     imageUrl: "images/restaurants/5c342ac9fc13ae39f8000001.jpg"
+  },
+  {
+    _id: "5c342ac9fc13ae39f8000003",
+    name: "Ramen Champion",
+    address:
+      "#03-89, 6 Eu Tong Sen Street, The Central, Clarke Quay, Lower Central, 059817 Singapore",
+    openingTime: "11:00",
+    closingTime: "22:00",
+    cuisine: { _id: "5c3430ecfc13ae122d000001", name: "Japanese" },
+    imageUrl: "images/restaurants/5c342ac9fc13ae39f8000003.jpg",
+    averagePrice: 20
+  },
+  {
+    _id: "5c342ac9fc13ae39f8000004",
+    name: "Umi Sushi (One Raffles Place)",
+    address:
+      "B1-24/25, 1 Raffles Place, OUB Centre, Clarke Quay, Lower Central, 048616 Singapore",
+    openingTime: "11:00",
+    closingTime: "20:00",
+    cuisine: { _id: "5c3430ecfc13ae122d000001", name: "Japanese" },
+    imageUrl: "images/restaurants/5c342ac9fc13ae39f8000004.jpg",
+    averagePrice: 15
   }
 ];
 
@@ -40,5 +62,18 @@ test("displays list of two restaurants on load", () => {
   const { getAllByText } = render(<HomePage />);
 
   expect(RestaurantService.getRestaurants).toHaveBeenCalledTimes(1);
-  expect(getAllByText("Order").length).toEqual(2);
+  expect(getAllByText("Order").length).toEqual(4);
+});
+
+test("Display only japanese rest if japanese is selected", () => {
+  const { getByText, queryByText, container } = render(<HomePage />);
+
+  const button = getByText(/japanese/i, "btn");
+  console.log(button);
+
+  fireEvent.click(button, { button: 1 });
+
+  expect(getByText(/ramen.*champion/i)).toBeInTheDocument();
+  expect(getByText(/Umi.*Sushi/i)).toBeInTheDocument();
+  expect(queryByText(/Soup.*Spoon/i)).not.toBeInTheDocument();
 });
