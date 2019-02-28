cd59c0b30b0eb88742268130ad55d6ccdc380609
nipunbatra1994@gmail.com <nipunbatra1994@gmail.com>
2019-02-28 15:58:59 +0800

    Add Restaurant File

--- src/components/AdminPage/AdminPage.jsx
+++ src/components/AdminPage/AdminPage.jsx
@@ -1,24 +1,35 @@
-import React, {Component} from "react";
-import RestaurantTable from "../RestaurantTable/RestaurantTable"
-import { getRestaurants, deleteRestaurant } from '../../services/restaurantService';
-
+import React, { Component } from "react";
+import RestaurantTable from "../RestaurantTable/RestaurantTable";
+import {
+  getRestaurants,
+  deleteRestaurant
+} from "../../services/restaurantService";
+import { Link } from "react-router-dom";
 class AdminPage extends Component {
   state = {
     restaurants: getRestaurants()
-  }
+  };
 
   handleDelete = restaurantId => {
-    deleteRestaurant(restaurantId)
+    deleteRestaurant(restaurantId);
     this.setState({ restaurants: getRestaurants() });
   };
 
-  render(){
-    const {restaurants} = this.state;
+  render() {
+    const { restaurants } = this.state;
     return (
       <div data-testid="admin-page">
-        <RestaurantTable restaurants={restaurants} handleDelete={this.handleDelete}/>
+        <div className="row justify-content-end">
+          <Link className="btn btn-primary" to="/restaurants/new">
+            Create New
+          </Link>
+        </div>
+        <RestaurantTable
+          restaurants={restaurants}
+          handleDelete={this.handleDelete}
+        />
       </div>
-    )
+    );
   }
 }
 
---
+++ src/components/RestaurantForm/RestaurantForm.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class RestaurantForm extends Component {
+  render() {
+    return <div>RestaurantForm</div>;
+  }
+}
+
+export default RestaurantForm;
